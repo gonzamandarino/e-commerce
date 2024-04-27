7ce0419b9681804f412cfd842c09bbcd7e8bef62
lvalletta <lucavalletta03@gmail.com>
2024-04-27 18:29:44 -0300

    alerta del checkout

--- e-commerce-biblio-uade/src/componentes/cart.jsx
+++ e-commerce-biblio-uade/src/componentes/cart.jsx
@@ -1,25 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../contexts/cartContext";
 
 const ShoppingCart = () => {
     const [cart, setCart] = useContext(CartContext);
+    const [showAlert, setShowAlert] = useState(false);
 
     const quantity = cart.reduce((acc, curr) => {
         return acc + curr.quantity;
     }, 0);
 
     const totalPrice = cart.reduce(
-    (acc, curr) => acc + curr.quantity * curr.precio,0);
+        (acc, curr) => acc + curr.quantity * curr.precio, 0);
+
+    const handleCheckout = () => {
+        // como enviar una solicitud al servidor para procesar la compra.
+        setShowAlert(true);
+        setCart([]);
+
+        // Ocultar la alerta después de 3 segundos
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 3000);
+    };
 
     return (
         <div className="cart-container">
+            {/* Mostrar la alerta solo si showAlert es true */}
+            {showAlert && (
+                <div className="alert alert-success alert-dismissible fade show" role="alert">
+                    ¡Compra realizada con éxito!
+                    <button type="button" className="btn-close" onClick={() => setShowAlert(false)}></button>
+                </div>
+            )}
             <div>
                 <div>Items in cart: {quantity}</div>
                 <div>Total: ${totalPrice}</div>
-                <button onClick={() => console.log(cart)}>Checkout</button>
+                <button onClick={handleCheckout} className="btn btn-primary">Checkout</button>
             </div>
         </div>
     );
 };
 
-export default ShoppingCart
+export default ShoppingCart;
