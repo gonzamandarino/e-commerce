078852516d2074912928e86e3d87eb79f0f180a2
AdrielFierro <adri.fierro@outlook.com>
2024-05-02 21:10:29 -0300

    add de getlibro y detalle libro(mejoras)

--- e-commerce-biblio-uade/src/componentes/DetalleLibro.jsx
+++ e-commerce-biblio-uade/src/componentes/DetalleLibro.jsx
@@ -1,15 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams,useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import getLibrosxId from "../service/getLibrosxId"
 
 
 export  const DetalleLibro = () =>{
 
+    const cadena = "/catalogo/1";
+    const indice = "/catalogo/".length;
+    const numero = cadena.substring(indice);
+    const [libros,setLibros]=useState([])
+    const location = useLocation();
+    console.log(numero);
+    console.log(location.pathname);
+
+    useEffect(() => {
+        getLibrosxId(numero).then((data) => {
+            setLibros(data);
+        });
+    }, []);
+
+
 const{DetalleLibro} =useParams();
 
 return(
+    
 <>
 
 
-<div>{DetalleLibro}</div>
+<div>{libros.nombre}</div>
 
 
 
--- e-commerce-biblio-uade/src/componentes/listaCards.jsx
+++ e-commerce-biblio-uade/src/componentes/listaCards.jsx
@@ -13,14 +13,14 @@ const ListaCard = () => {
         });
     }, []);
 
+    const [showView1, setShowView1] = useState(true);
+
+
 
 
 
     return (
-
-     
       
-
         <div className="container-fluid">
         <div className="row justify-content-center">
           
@@ -28,18 +28,18 @@ const ListaCard = () => {
             <div className="col-md-4 col-sm-6 mb-3" key={product.id}>
             <div className="card my-3 py-3 border-0">
               <Card {...product} />
+              
             </div>
           </div>
           ))}
         </div>
           
       </div>
-
       
-
         
       
           );
+          
 };
 
 export default ListaCard
